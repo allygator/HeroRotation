95d87151ac5e179cbfc19bf34332dc10e8315506
allygator <allygator95@gmail.com>
2016-04-19 20:25:31 -0400

    Hero Images
    Hero names show images

--- heroes.js
+++ heroes.js
@@ -1,4 +1,249 @@
+$("#abathur").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/abathur.jpg) right no-repeat")
+});
+
+$("#anubarak").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/anubarak.jpg) right no-repeat")
+});
+
+$("#artanis").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/artanis.jpg) right no-repeat")
+});
+
+$("#arthas").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/arthas.jpg) right no-repeat")
+});
+
+$("#azmodan").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/azmodan.jpg) right no-repeat")
+});
+
+$("#brightwing").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/brightwing.jpg) right no-repeat")
+});
+
+$("#chen").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/chen.jpg) right no-repeat")
+});
+
+$("#chogall").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/chogall.jpg) right no-repeat")
+});
+
+$("#dehaka").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/dehaka.jpg) right no-repeat")
+});
+
+$("#diablo").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/diablo.jpg) right no-repeat")
+});
+
+$("#etc").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/etc.jpg) right no-repeat")
+});
+
+$("#falstad").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/falstad.jpg) right no-repeat")
+});
+
+$("#gazlowe").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/gazlowe.jpg) right no-repeat")
+});
+
+$("#greymane").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/greymane.jpg) right no-repeat")
+});
+
+$("#illidan").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/illidan.jpg) right no-repeat")
+});
+
+$("#jaina").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/jaina.jpg) right no-repeat")
+});
+
+$("#johanna").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/johanna.jpg) right no-repeat")
+});
+
+$("#kaelthas").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/kaelthas.jpg) right no-repeat")
+});
+
+$("#kerrigan").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/kerrigan.jpg) right no-repeat")
+});
+
+$("#kharazim").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/kharazim.jpg) right no-repeat")
+});
+
+$("#leoric").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/leoric.jpg) right no-repeat")
+});
+
+$("#lili").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/lili.jpg) right no-repeat")
+});
+
+$("#liming").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/liming.jpg) right no-repeat")
+});
+
+$("#ltmorales").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/ltmorales.jpg) right no-repeat")
+});
+
+$("#lunara").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/lunara.jpg) right no-repeat")
+});
+
+$("#malfurion").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/malfurion.jpg) right no-repeat")
+});
+
+$("#muradin").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/muradin.jpg) right no-repeat")
+});
+
+$("#murky").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/murky.jpg) right no-repeat")
+});
+
+$("#nazeebo").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/nazeebo.jpg) right no-repeat")
+});
+
+$("#nova").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/nova.jpg) right no-repeat")
+});
+
 $("#raynor").click(function(){
 	$("#heroimage").css("height", "751px")
     $("#heroimage").css("background", "url(assets/raynor.jpg) right no-repeat")
+});
+
+$("#rehgar").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/rehgar.jpg) right no-repeat")
+});
+
+$("#rexxar").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/rexxar.jpg) right no-repeat")
+});
+
+$("#sgthammer").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/sgthammer.jpg) right no-repeat")
+});
+
+$("#sonya").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/sonya.jpg) right no-repeat")
+});
+
+$("#stitches").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/stitches.jpg) right no-repeat")
+});
+
+$("#sylvanas").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/sylvanas.jpg) right no-repeat")
+});
+
+$("#tassadar").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/tassadar.jpg) right no-repeat")
+});
+
+$("#thebutcher").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/thebutcher.jpg) right no-repeat")
+});
+
+$("#thelostvikings").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/thelostvikings.jpg) right no-repeat")
+});
+
+$("#thrall").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/thrall.jpg) right no-repeat")
+});
+
+$("#tracer").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/tracer.jpg) right no-repeat")
+});
+
+$("#tychus").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/tychus.jpg) right no-repeat")
+});
+
+$("#tyrael").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/tyrael.jpg) right no-repeat")
+});
+
+$("#tyrande").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/tyrande.jpg) right no-repeat")
+});
+
+$("#uther").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/uther.jpg) right no-repeat")
+});
+
+$("#valla").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/valla.jpg) right no-repeat")
+});
+
+$("#xul").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/xul.jpg) right no-repeat")
+});
+
+$("#zagara").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/zagara.jpg) right no-repeat")
+});
+
+$("#zeratul").click(function(){
+	$("#heroimage").css("height", "751px")
+    $("#heroimage").css("background", "url(assets/zeratul.jpg) right no-repeat")
 });
