ff7a261811091238e1a7aa04af8ced936a20f45e
Alyssa <allygator95@gmail.com>
2016-09-28 20:15:13 -0400

    Revert "Added greymane images, updated heroes.js"
    This reverts commit 51a4d5e9f5d9c44ff1e95266002bc5b4c9fed4ec.

--- runJava.sh
+++ runJava.sh
@@ -2,5 +2,4 @@
  java -jar PageCrawler.jar
  git config --global user.name "Alyssa"
  git config --global user.email "allygator95@gmail.com"
- git add heroes.js
- git commit -m "Updated heroes.js"
+ $SHELL
